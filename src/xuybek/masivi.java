f527b6d407cb50583ea4fe9c8cac69e25cf8e065
DESKTOP-QPKEOAL\yusif <yusif_aliyev_2016@mail.ru>
2022-12-06 21:16:08 +0400

    no message

---
+++ src/xuybek/masivi.java
@@ -0,0 +1,25 @@
+package xuybek;
+
+        public class masivi {
+
+
+            public static void main (String[] args) {
+
+                int[] nums = new int[6];
+                nums[0] = 45;
+                nums[1] = 124;
+                nums[2] = 44355;
+                nums[3] = 3455;
+                nums[4] = 8765;
+                nums[5] = 678675;
+                int res = nums[2] + nums[5];
+
+
+                System.out.println(res);
+
+                float[] flex2 = new float[]{5.0f, 18.345f, 10.03f};
+                for(int i = 0; i < flex2.length; i++)
+                    System.out.println("sueta " + flex2[i]);
+            }
+
+            }
---
+++ src/xuybek/minimalniy.java
@@ -0,0 +1,24 @@
+package xuybek;
+import java.util.Scanner;
+
+public class minimalniy {
+
+    public static void main (String[] args) {
+        int[] sueta = new int[5];
+        Scanner scanner = new Scanner(System.in);
+
+        for(int e = 0; e < sueta.length; e++) {
+            System.out.print("бидяня номря яз: ");
+            int value = scanner .nextInt();
+            sueta[e] = value;
+
+            int minimal = sueta[0];
+            for(int i = 0; i < sueta.length; i++) {
+                if(sueta[i] < minimal)
+                    minimal = sueta[i];
+                System.out.println("Минимальное число: " + minimal);
+                }
+            }
+        }
+    }
+
